672b230b7fd4afd471995d276d4181ffc1a1bea2
Tatyana Kudiyarova <tatyana.kudiyarova@gmail.com>
2014-05-01 16:18:17 +1200

    using new hash syntax

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,4 +1,4 @@
 class Timesheet < ActiveRecord::Base
-  belongs_to :user,    :inverse_of => :timesheets
-  belongs_to :project, :inverse_of => :timesheets
+  belongs_to :user,    inverse_of: :timesheets
+  belongs_to :project, inverse_of: :timesheets
 end
